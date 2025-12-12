b0a381f22979ae751a78318d60ae61cbac717780
Aliza's <aliza.visuals@gmail.com>
2025-12-12 15:26:59 -0600

    GetAllCategories method body created.

--- backend-api/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ backend-api/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,59 +5,75 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
-{
-    public MySqlCategoryDao(DataSource dataSource)
-    {
+public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
+    public MySqlCategoryDao(DataSource dataSource) {
         super(dataSource);
     }
 
     @Override
-    public List<Category> getAllCategories()
-    {
-        // get all categories
-        return null;
+    public List<Category> getAllCategories() {
+
+        // the array list that will hold the category objects
+        ArrayList<Category> categories = new ArrayList<>();
+        // getting all column under categories
+        String sql = "SELECT * FROM categories;";
+
+        try (
+                // using the getconnection() method from MySqlDaoBase - cant directly use datasource cause its private
+                Connection connection = getConnection();
+                // preparing and executing the statment
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                ResultSet resultSet = preparedStatement.executeQuery();)
+        {
+            while (resultSet.next()) {
+                // converts row into category object
+                Category category = mapRow(resultSet);
+                // stores it inside the array list
+                categories.add(category);
+            }
+        } catch (SQLException e) {
+            System.err.println("Error while running getAllCategories(): " + e.getMessage());
+        }
+        return categories;
+
     }
 
     @Override
-    public Category getById(int categoryId)
-    {
+    public Category getById(int categoryId) {
         // get category by id
         return null;
     }
 
     @Override
-    public Category create(Category category)
-    {
+    public Category create(Category category) {
         // create a new category
         return null;
     }
 
     @Override
-    public void update(int categoryId, Category category)
-    {
+    public void update(int categoryId, Category category) {
         // update category
     }
 
     @Override
-    public void delete(int categoryId)
-    {
+    public void delete(int categoryId) {
         // delete category
     }
 
-    private Category mapRow(ResultSet row) throws SQLException
-    {
+    private Category mapRow(ResultSet row) throws SQLException {
         int categoryId = row.getInt("category_id");
         String name = row.getString("name");
         String description = row.getString("description");
 
-        Category category = new Category()
-        {{
+        Category category = new Category() {{
             setCategoryId(categoryId);
             setName(name);
             setDescription(description);
