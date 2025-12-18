38b45beb29106e6dbd088ba34c025cf840e422f5
Aliza's <aliza.visuals@gmail.com>
2025-12-17 22:16:30 -0600

    updated the catController

--- backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
+++ backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -12,16 +12,20 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
+// tells spring that this class handles http requests and returns JSON
 @RestController
 @RequestMapping("categories")
 // this will set this as the baseURL for every method in this controller
+// every method path is relative to /categories
 // http://localhost:8080/categories
 @CrossOrigin
-// add annotation to allow cross site origin requests
+// add annotation to allow cross site origin requests ( frontend apps )
 public class CategoriesController {
+    // final so they never change after construction
     private final CategoryDao categoryDao;
     private final ProductDao productDao;
 
+    // spring inject the DAO implementations
     // create an Autowired controller to inject the categoryDao and ProductDao
     // constructor injection
     @Autowired
@@ -30,57 +34,72 @@ public class CategoriesController {
         this.categoryDao = categoryDao;
         this.productDao = productDao;
     }
-    // add the appropriate annotation for a get action
 
-
+    // handles get / categories - no path var and no request body
     @GetMapping
     public List<Category> getAll() {
-        // find and return all categories
+        // find and return the categories list
+        // the list is converted into JSON
+        // HTTP status is set to 200
         return categoryDao.getAllCategories();
     }
 
 
-    // add the appropriate annotation for a get action
     @GetMapping("/{id}")
     public Category getByCategoryId(@PathVariable int id) {
-        // get the category by id
+        // spring takes the id from the URL and converts it to data
         Category category = categoryDao.getByCategoryID(id);
 
         if (category == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category" + id + "not found.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category " + id + " not found.");
         }
+        // cat is returned in JSON
         return category;
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
-     @GetMapping("{categoryId}/products")
-        public List<Product> getProductsById(@PathVariable int categoryId) {
+    @GetMapping("{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId) {
+        // Sspring takes the id from URL
+        // looks for products for catId
+        // returns list of products in JSON
         return productDao.listByCategoryId(categoryId);
-     }
-//    }
+    }
 
-    // add annotation to call this method for a POST action
+
     @PostMapping
-    @ResponseStatus (value =  HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.CREATED)
     // add annotation to ensure that only an ADMIN can call this function
     @PreAuthorize("hasRole('ADMIN')")
+    // HTTP request contains body ( includes name and description )
+    // spring reads the JSON body and creates a java obj
     public Category addCategory(@RequestBody Category category) {
-        // insert the category
+        // inserts obj into DB, get gen id, gets row, returns full cat
         return categoryDao.create(category);
     }
+
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     @PutMapping("/{id}")
     // add annotation to ensure that only an ADMIN can call this function
     @PreAuthorize("hasRole('ADMIN')")
+    // HTTP request contains body ( includes cat id, name and description )
+    // spring extracts the id
+    // converts JSON into cat obj
     public void updateCategory(@PathVariable int id, @RequestBody Category category) {
 
+        // java check the ID from URL VS ID inside the request body
         if (id != category.getCategoryId()) {
-            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "The categoryID is different from categoryID in the body");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The categoryID in URL is different from categoryID in the body");
         }
-        // update the category by id
-        categoryDao.update(id, category);
+        // call dao and store the result
+        boolean updated = categoryDao.update(id, category);
+
+        // if dao says nothing was updated, category does not exist
+        if (!updated) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category " + id + " not found.");
+        }
     }
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
@@ -88,8 +107,13 @@ public class CategoriesController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     // add annotation to ensure that only an ADMIN can call this function
     @PreAuthorize("hasRole('ADMIN')")
+
     public void deleteCategory(@PathVariable int id) {
-        // delete the category by id
-        categoryDao.delete(id);
+        // call dao deleted method and store the result, if true cat is deleted, if false no cat existed
+        boolean deleted = categoryDao.delete(id);
+
+        if (!deleted) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category " + id + " not found.");
+        }
     }
 }
